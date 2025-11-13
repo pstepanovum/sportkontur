bc5e760e9f1ad7e5928676c96d76a8ce4d7d1d50
Pavel Stepanov <pas273@miami.edu>
2025-11-13 12:43:46 -0500

    fix: correct import statements for CTA and SectionWrapper components

--- src/app/catalog/[category]/[id]/page.tsx
+++ src/app/catalog/[category]/[id]/page.tsx
@@ -2,9 +2,9 @@
 
 import Header from "@/components/ui/sections/header";
 import Footer from "@/components/ui/sections/footer";
-import { CTA } from "@/components/ui/sections/cta";
+import CTA from "@/components/ui/sections/cta";
 import { BlurWrapper } from "@/components/ui/blur-wrapper";
-import SectionWrapper from "@/components/ui/sections/helper/section-wrapper";
+import {SectionWrapper} from "@/components/ui/sections/helper/section-wrapper";
 import ProductDetail from "@/components/pages/catalog/product-detail";
 
 interface ProductPageProps {
